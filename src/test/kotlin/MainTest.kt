a0c40fd1ed1a5830f769513767fcbdee6b3b9fd3
hrtwt <29752757+hrtwt@users.noreply.github.com>
2025-03-20 22:53:10 +0900

    integrate lexer and parser

--- src/test/kotlin/MainTest.kt
+++ src/test/kotlin/MainTest.kt
@@ -1,12 +1,15 @@
+import org.assertj.core.api.Assertions.assertThat
+import org.hrtwt.Token
+import org.hrtwt.Type
+import org.hrtwt.ast.Number
+import org.hrtwt.ast.Plus
 import org.hrtwt.main
 import org.junit.jupiter.api.Test
-import org.assertj.core.api.Assertions.assertThat
 
 class MainTest {
 
     @Test
-    fun sample(){
-        val main = main()
-        assertThat(true).isTrue()
+    fun test() {
+        assertThat(main("1+ 2")).isEqualTo(Plus(Number(Token(Type.NUMBER, "1")), Number(Token(Type.NUMBER, "2"))))
     }
 }
